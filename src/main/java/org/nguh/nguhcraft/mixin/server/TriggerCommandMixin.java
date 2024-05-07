46b23bf1fbb25bf6472c60dcdac265103b259935
Sirraide <aeternalmail@gmail.com>
2024-05-07 21:05:48 +0200

    [Server] Remove /trigger command

---
+++ src/main/java/org/nguh/nguhcraft/mixin/server/TriggerCommandMixin.java
@@ -0,0 +1,17 @@
+package org.nguh.nguhcraft.mixin.server;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.command.TriggerCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(TriggerCommand.class)
+public class TriggerCommandMixin {
+    /**
+    * @author Sirraide
+    * @reason It’s unnecessary, and removing it cleans up the command list a bit.
+    */
+    @Overwrite
+    public static void register(CommandDispatcher<ServerCommandSource> D) {}
+}
