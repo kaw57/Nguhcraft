827d0547a607db19dcf7ea45a1b1d6bad5aa5679
Sirraide <aeternalmail@gmail.com>
2024-05-16 13:00:07 +0200

    [Server] Prevent endermen from picking up blocks

---
+++ src/main/java/org/nguh/nguhcraft/mixin/server/EndermanEntityMixin.java
@@ -0,0 +1,22 @@
+package org.nguh.nguhcraft.mixin.server;
+
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.entity.mob.EndermanEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EndermanEntity.class)
+public abstract class EndermanEntityMixin {
+    /** Just hijack the call that adds the PickupBlockGoal to do nothing. */
+    @Redirect(
+        method = "initGoals()V",
+        at = @At(
+            value = "INVOKE",
+            target = "net/minecraft/entity/ai/goal/GoalSelector.add (ILnet/minecraft/entity/ai/goal/Goal;)V",
+            ordinal = 7
+        )
+    )
+    private void inject$initGoals(GoalSelector instance, int priority, Goal goal) {}
+}
