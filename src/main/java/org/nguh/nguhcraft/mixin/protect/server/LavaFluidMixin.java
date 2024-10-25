f1175e9de564835bf2b18275621c9d0aa8169e78
Sirraide <aeternalmail@gmail.com>
2024-10-25 19:07:45 +0200

    [Regions] Disable fire spread from lava

---
+++ src/main/java/org/nguh/nguhcraft/mixin/protect/server/LavaFluidMixin.java
@@ -0,0 +1,22 @@
+package org.nguh.nguhcraft.mixin.protect.server;
+
+import net.minecraft.fluid.LavaFluid;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
+import org.nguh.nguhcraft.protect.ProtectionManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LavaFluid.class)
+public abstract class LavaFluidMixin {
+    @Inject(method = "hasBurnableBlock", at = @At("HEAD"), cancellable = true)
+
+    /** Disable lava lighting protected blocks on fire. */
+    private void onHasBurnableBlock(WorldView WV, BlockPos Pos, CallbackInfoReturnable<Boolean> CIR) {
+        if (!(WV instanceof ServerWorld SW)) return; // Ignore during world generation.
+        if (ProtectionManager.IsProtectedBlock(SW, Pos)) CIR.setReturnValue(false);
+    }
+}
