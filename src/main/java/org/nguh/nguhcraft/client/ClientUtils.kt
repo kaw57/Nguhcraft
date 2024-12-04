1f22d02d6353469b7e598bd236a38942938f542a
Sirraide <aeternalmail@gmail.com>
2024-12-04 13:05:34 +0100

    [Vanish] Show vanish indicator

--- src/main/java/org/nguh/nguhcraft/client/ClientUtils.kt
+++ src/main/java/org/nguh/nguhcraft/client/ClientUtils.kt
@@ -3,7 +3,14 @@ package org.nguh.nguhcraft.client
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.util.math.MatrixStack
 import java.text.Normalizer
+
+inline fun MatrixStack.Push(Transformation: MatrixStack.() -> Unit) {
+    push()
+    Transformation()
+    pop()
+}
 
 @Environment(EnvType.CLIENT)
 object ClientUtils {
--- src/main/java/org/nguh/nguhcraft/client/NguhcraftClient.kt
+++ src/main/java/org/nguh/nguhcraft/client/NguhcraftClient.kt
@@ -10,7 +10,9 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents
+import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
@@ -18,10 +20,12 @@ import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.server.integrated.IntegratedServer
 import net.minecraft.text.Text
+import net.minecraft.util.Colors
 import net.minecraft.util.Formatting
 import net.minecraft.util.math.BlockPos
 import org.nguh.nguhcraft.Nguhcraft.Companion.Id
 import org.nguh.nguhcraft.block.NguhBlocks
+import org.nguh.nguhcraft.client.ClientUtils.Client
 
 @Environment(EnvType.CLIENT)
 class NguhcraftClient : ClientModInitializer {
@@ -29,7 +33,11 @@ class NguhcraftClient : ClientModInitializer {
         ClientNetworkHandler.Init()
 
         WorldRenderEvents.BEFORE_DEBUG_RENDER.register { RegionRenderer.Render(it) }
-        HudRenderCallback.EVENT.register { Ctx, _ -> RegionRenderer.RenderHUD(Ctx) }
+        HudRenderCallback.EVENT.register {
+            Ctx, _ ->
+            RegionRenderer.RenderHUD(Ctx)
+            RenderVanishedMessage(Ctx)
+        }
 
         Registry.register(Registries.ITEM_GROUP, Id("treasures"), TREASURES_ITEM_GROUP)
 
@@ -47,6 +55,9 @@ class NguhcraftClient : ClientModInitializer {
             .entries {  Ctx, Entries -> Treasures.AddAll(Ctx, Entries) }
             .build()
 
+        const val VANISH_MSG = "You are currently vanished"
+
+        // FIXME: These should probably not be here.
         @JvmField @Volatile var InHypershotContext = false
         @JvmField @Volatile var BypassesRegionProtection = false
         @JvmField @Volatile var Vanished = false
@@ -80,5 +91,18 @@ class NguhcraftClient : ClientModInitializer {
                     0
                 }
             )
+
+        fun RenderVanishedMessage(Ctx: DrawContext) {
+            if (!Vanished) return
+            val TR = Client().textRenderer
+            Ctx.drawText(
+                TR,
+                VANISH_MSG,
+                Ctx.scaledWindowWidth  - TR.getWidth(VANISH_MSG) - 5,
+                TR.getWrappedLinesHeight(VANISH_MSG, 10000) - 5,
+                Colors.LIGHT_YELLOW,
+                true
+            )
+        }
     }
 }
