7941cd14fff4f0d356d7c22970f9b7787c3ea2f9
Sirraide <aeternalmail@gmail.com>
2024-12-15 20:22:52 +0100

    [Items] Add new armour trims

--- src/main/java/org/nguh/nguhcraft/item/NguhItems.kt
+++ src/main/java/org/nguh/nguhcraft/item/NguhItems.kt
@@ -3,6 +3,7 @@ package org.nguh.nguhcraft.item
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroups
+import net.minecraft.item.SmithingTemplateItem
 import net.minecraft.recipe.SpecialCraftingRecipe.SpecialRecipeSerializer
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
@@ -15,11 +16,11 @@ import org.nguh.nguhcraft.Nguhcraft.Companion.Id
 object NguhItems {
     val LOCK: Item = CreateItem(LockItem.ID, LockItem())
     val KEY: Item = CreateItem(KeyItem.ID, KeyItem())
-    val SLABLET_1: Item = CreateItem(Id("slablet_1"), Item.Settings().maxCount(64).rarity(Rarity.RARE).fireproof())
-    val SLABLET_2: Item = CreateItem(Id("slablet_2"), Item.Settings().maxCount(64).rarity(Rarity.RARE).fireproof())
-    val SLABLET_4: Item = CreateItem(Id("slablet_4"), Item.Settings().maxCount(64).rarity(Rarity.RARE).fireproof())
-    val SLABLET_8: Item = CreateItem(Id("slablet_8"), Item.Settings().maxCount(64).rarity(Rarity.RARE).fireproof())
-    val SLABLET_16: Item = CreateItem(Id("slablet_16"), Item.Settings().maxCount(64).rarity(Rarity.RARE).fireproof())
+    val SLABLET_1: Item = CreateItem(Id("slablet_1"), Item.Settings().maxCount(64).rarity(Rarity.UNCOMMON).fireproof())
+    val SLABLET_2: Item = CreateItem(Id("slablet_2"), Item.Settings().maxCount(64).rarity(Rarity.UNCOMMON).fireproof())
+    val SLABLET_4: Item = CreateItem(Id("slablet_4"), Item.Settings().maxCount(64).rarity(Rarity.UNCOMMON).fireproof())
+    val SLABLET_8: Item = CreateItem(Id("slablet_8"), Item.Settings().maxCount(64).rarity(Rarity.UNCOMMON).fireproof())
+    val SLABLET_16: Item = CreateItem(Id("slablet_16"), Item.Settings().maxCount(64).rarity(Rarity.UNCOMMON).fireproof())
     val NGUHROVISION_2024_DISC: Item = CreateItem(
         Id("music_disc_nguhrovision_2024"),
         Item.Settings()
@@ -27,6 +28,11 @@ object NguhItems {
             .rarity(Rarity.EPIC)
             .jukeboxPlayable(RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Id("nguhrovision_2024")))
     )
+
+    val ATLANTIC_ARMOUR_TRIM: Item = CreateSmithingTemplate("atlantic_armour_trim_smithing_template", Item.Settings().rarity(Rarity.RARE))
+    val CENRAIL_ARMOUR_TRIM: Item = CreateSmithingTemplate("cenrail_armour_trim_smithing_template", Item.Settings().rarity(Rarity.RARE))
+    val ICE_COLD_ARMOUR_TRIM: Item = CreateSmithingTemplate("ice_cold_armour_trim_smithing_template", Item.Settings().rarity(Rarity.RARE))
+    val VENEFICIUM_ARMOUR_TRIM: Item = CreateSmithingTemplate("veneficium_armour_trim_smithing_template", Item.Settings().rarity(Rarity.RARE))
 
     fun Init() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register {
@@ -41,6 +47,13 @@ object NguhItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register {
             it.add(NGUHROVISION_2024_DISC)
+        }
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register {
+            it.add(ATLANTIC_ARMOUR_TRIM)
+            it.add(CENRAIL_ARMOUR_TRIM)
+            it.add(ICE_COLD_ARMOUR_TRIM)
+            it.add(VENEFICIUM_ARMOUR_TRIM)
         }
 
         KeyLockPairingRecipe.SERIALISER = Registry.register(
@@ -61,4 +74,15 @@ object NguhItems {
 
     private fun CreateItem(S: Identifier, I: Item.Settings): Item =
         Registry.register(Registries.ITEM, S, Item(I.registryKey(RegistryKey.of(RegistryKeys.ITEM, S))))
+
+    private fun CreateSmithingTemplate(S: String, I: Item.Settings): Item {
+        val Id = Id(S)
+        return Registry.register(
+            Registries.ITEM,
+            Id,
+            SmithingTemplateItem.of(
+                I.registryKey(RegistryKey.of(RegistryKeys.ITEM, Id))
+            )
+        )
+    }
 }
