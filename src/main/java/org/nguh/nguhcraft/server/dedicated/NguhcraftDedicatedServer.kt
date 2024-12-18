27920e7025c883beb5d80a40d6e2e95be8f15cbd
Sirraide <aeternalmail@gmail.com>
2024-12-18 18:06:45 +0100

    [Console] Coloured log messages!

--- src/main/java/org/nguh/nguhcraft/server/dedicated/NguhcraftDedicatedServer.kt
+++ src/main/java/org/nguh/nguhcraft/server/dedicated/NguhcraftDedicatedServer.kt
@@ -1,20 +1,56 @@
 package org.nguh.nguhcraft.server.dedicated
 
+import com.mojang.logging.LogUtils
 import net.fabricmc.api.DedicatedServerModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.server.dedicated.MinecraftDedicatedServer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.LoggerContext
+import org.apache.logging.log4j.core.appender.rewrite.RewriteAppender
 
 @Environment(EnvType.SERVER)
 class NguhcraftDedicatedServer : DedicatedServerModInitializer {
+    val LOGGER = LogUtils.getLogger()
+
     override fun onInitializeServer() {
+        // The stack deobfuscator likes to replace our appender, so replace
+        // it again, but make sure to grab the rewriter so we can use it.
+        if (FabricLoader.getInstance().isModLoaded(STACK_DEOBFUSCATOR_MOD_ID)) {
+            val Root = LogManager.getRootLogger()
+            val Ctx = LogManager.getContext(false) as LoggerContext
+            val Config = Ctx.configuration.getLoggerConfig(Root.name)
+            val Rewriter = Config.appenders[STACK_DEOBFUSCATOR_APPENDER_NAME] as? RewriteAppender
+            if (Rewriter != null) {
+                ReplaceLogger(STACK_DEOBFUSCATOR_APPENDER_NAME, Rewriter)
+                LOGGER.info("Successfully wrapped stack deobfuscator!")
+            }
+        }
+
         ServerLifecycleEvents.SERVER_STARTING.register {
             Discord.Start(it as MinecraftDedicatedServer)
         }
 
         ServerLifecycleEvents.SERVER_STOPPING.register {
             Discord.Stop()
+        }
+    }
+
+    companion object {
+        const val STACK_DEOBFUSCATOR_APPENDER_NAME = "StackDeobfAppender"
+        const val STACK_DEOBFUSCATOR_MOD_ID = "stackdeobfuscator"
+
+        internal fun ReplaceLogger(ToReplace: String, Rewriter: RewriteAppender? = null) {
+            val Root = LogManager.getRootLogger()
+            val Ctx = LogManager.getContext(false) as LoggerContext
+            val Config = Ctx.configuration.getLoggerConfig(Root.name)
+            val A = NguhcraftAppender(Rewriter)
+            A.start()
+            Config.removeAppender(ToReplace)
+            Config.addAppender(A, Config.level, null)
+            Ctx.updateLoggers()
         }
     }
 }
---
+++ src/main/java/org/nguh/nguhcraft/server/dedicated/NguhcraftDedicatedServerPreLaunch.kt
@@ -0,0 +1,85 @@
+package org.nguh.nguhcraft.server.dedicated
+
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.core.LogEvent
+import org.apache.logging.log4j.core.appender.AbstractAppender
+import org.apache.logging.log4j.core.appender.rewrite.RewriteAppender
+import org.apache.logging.log4j.core.appender.rewrite.RewritePolicy
+import org.apache.logging.log4j.core.layout.PatternLayout
+import org.nguh.nguhcraft.server.ServerUtils.IsDedicatedServer
+import org.nguh.nguhcraft.server.dedicated.NguhcraftDedicatedServer.Companion.ReplaceLogger
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+
+@Environment(EnvType.SERVER)
+class NguhcraftAppender(Rewriter: RewriteAppender? = null) : AbstractAppender(
+    "Nguhcraft",
+    null,
+    PatternLayout.newBuilder().build(),
+    false,
+    arrayOf()
+) {
+    val Policy = run {
+        if (Rewriter == null) return@run null
+        val Field = Rewriter.javaClass.getDeclaredField("rewritePolicy")
+        Field.isAccessible = true
+        return@run Field.get(Rewriter) as RewritePolicy
+    }
+
+    val ClassNameRewriter = run {
+        if (Policy == null) return@run null
+        val MappingsField = Policy.javaClass.getDeclaredField("mappings")
+        val RemapString = MappingsField.type.getDeclaredMethod("remapString", String::class.java)
+        MappingsField.isAccessible = true
+        RemapString.isAccessible = true
+        val Mappings = MappingsField.get(Policy)
+        return@run { Raw: String -> RemapString.invoke(Mappings, Raw) as String }
+    }
+
+    override fun append(Raw: LogEvent) {
+        if (Raw.level == Level.TRACE || Raw.level == Level.DEBUG) return
+        val E = Policy?.rewrite(Raw) ?: Raw
+        val T = LocalDateTime.ofInstant(Instant.ofEpochSecond(E.instant.epochSecond), ZoneOffset.systemDefault())
+        val C = E.source.className
+        var Class = (ClassNameRewriter?.invoke(C) ?: C).split('.').last()
+        if (Class.endsWith("\$Companion")) Class = Class.dropLast("\$Companion".length)
+        print(
+            "\u001b[34m[${T.format(DTF)}]\u001b[m " +
+            "\u001b[${ThreadFormat(E.level)}m[${E.threadName}/${E.level}]\u001b[m " +
+            "\u001b[36m($Class)\u001b[m " +
+            "\u001b[${MsgFormat(E.level)}m${layout.toSerializable(E)}\u001b[m"
+        )
+    }
+
+    companion object {
+        val DTF: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+
+        private fun MsgFormat(L: Level) = when (L) {
+            Level.FATAL, Level.ERROR -> "1;31"
+            Level.WARN -> "33"
+            else -> ""
+        }
+
+        private fun ThreadFormat(L: Level) = when (L) {
+            Level.FATAL, Level.ERROR -> "31"
+            Level.WARN -> "33"
+            else -> "32"
+        }
+    }
+}
+
+class NguhcraftDedicatedServerPreLaunch : PreLaunchEntrypoint {
+    /** Replace the default appender with one that adds pretty colours. */
+    override fun onPreLaunch() {
+        if (IsDedicatedServer()) {
+            println("[Nguhcraft] [PreLaunch] Setting up formatted logger...")
+            ReplaceLogger("SysOut")
+        }
+    }
+}
