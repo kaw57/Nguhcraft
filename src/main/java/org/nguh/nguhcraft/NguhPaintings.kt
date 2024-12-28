3ef378c428fdb1a510e354ebb5db75398f54b067
sq <134925668+SquareScreamYT@users.noreply.github.com>
2024-12-28 12:15:35 +0800

    [Client] Update rail diagram and add more paintings (#32)

--- src/main/java/org/nguh/nguhcraft/NguhPaintings.kt
+++ src/main/java/org/nguh/nguhcraft/NguhPaintings.kt
@@ -11,15 +11,17 @@ import org.nguh.nguhcraft.Nguhcraft.Companion.RKey
 import java.util.Optional
 
 object NguhPaintings {
+    val CHILLVANA_METRO = of("chillvana_metro")
     val GAMBIANHOLIDAY = of("gambianholiday")
     val GOLD_NGUH = of("gold_nguh")
     val GOLD_NGUH_SMALL = of("gold_nguh_small")
     val MAP = of("map")
     val RABO = of("rabo")
     val RAIL_DIAGRAM = of("rail_diagram")
+    val RAILS_OF_ERAS = of("rails_of_eras")
 
     // All paintings that can be obtained randomly by placing them.
-    val PLACEABLE = arrayOf(GAMBIANHOLIDAY, GOLD_NGUH, GOLD_NGUH_SMALL, MAP, RABO, RAIL_DIAGRAM)
+    val PLACEABLE = arrayOf(CHILLVANA_METRO, GAMBIANHOLIDAY, GOLD_NGUH, GOLD_NGUH_SMALL, MAP, RABO, RAIL_DIAGRAM)
 
     fun Bootstrap(R: Registerable<PaintingVariant>) {
         fun Register(K: RegistryKey<PaintingVariant>, Width: Int, Height: Int) {
@@ -32,12 +34,14 @@ object NguhPaintings {
             ))
         }
 
+        Register(CHILLVANA_METRO, 3, 2)
         Register(GAMBIANHOLIDAY, 1, 1)
         Register(GOLD_NGUH, 1, 1)
         Register(GOLD_NGUH_SMALL, 1, 1)
         Register(MAP, 5, 5)
         Register(RABO, 1, 4)
         Register(RAIL_DIAGRAM, 3, 3)
+        Register(RAILS_OF_ERAS, 3, 2)
     }
 
     fun of(Key: String) = RKey(RegistryKeys.PAINTING_VARIANT, Key)
