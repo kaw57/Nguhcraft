6eed1cbb88574642b120220f507f652fedee59bc
Sirraide <aeternalmail@gmail.com>
2025-02-06 17:48:19 +0100

    [Client] Disable social interactions screen
    Attempting to open this screen was already crashing the client
    because we remove the report button, so just prevent it from
    opening entirely.
    
    Fixes #45.

--- src/main/java/org/nguh/nguhcraft/mixin/client/MinecraftClientMixin.java
+++ src/main/java/org/nguh/nguhcraft/mixin/client/MinecraftClientMixin.java
@@ -1,14 +1,19 @@
 package org.nguh.nguhcraft.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.SocialInteractionsScreen;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.session.ProfileKeys;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.Nullable;
 import org.nguh.nguhcraft.client.NguhcraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,8 +21,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
     @Shadow @Nullable public ClientPlayerEntity player;
-
     @Shadow @Nullable public ClientWorld world;
+    @Unique static private final Text REPORT_SCREEN_DISABLED =
+        Text.literal("Social interactions screen is not supported by Nguhcraft!").formatted(Formatting.RED);
 
     /** Prevent using an item while in a hypershot context. */
     @Inject(method = "doItemUse()V", at = @At("HEAD"), cancellable = true)
@@ -42,4 +48,13 @@ public abstract class MinecraftClientMixin {
     * */
     @Overwrite
     public boolean isTelemetryEnabledByApi() { return false; }
+
+    /** Prevent the social interactions screen from opening. */
+    @Inject(method = "setScreen", at = @At("HEAD"), cancellable = true)
+    private void inject$setScreen$0(Screen S, CallbackInfo CI) {
+        if (S instanceof SocialInteractionsScreen) {
+            if (player != null) player.sendMessage(REPORT_SCREEN_DISABLED, false);
+            CI.cancel();
+        }
+    }
 }
--- src/main/java/org/nguh/nguhcraft/mixin/client/render/GameMenuScreenMixin.java
+++ src/main/java/org/nguh/nguhcraft/mixin/client/render/GameMenuScreenMixin.java
@@ -14,7 +14,21 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(GameMenuScreen.class)
 public abstract class GameMenuScreenMixin {
-    @Unique static private final Tooltip TT = Tooltip.of(Text.of("LAN multiplayer is not supported by Nguhcraft!"));
+    @Unique static private final Tooltip LAN_DISABLED = Tooltip.of(Text.of("LAN multiplayer is not supported by Nguhcraft!"));
+    @Unique static private final Tooltip REPORT_SCREEN_DISABLED = Tooltip.of(Text.of("Social interactions screen is not supported by Nguhcraft!"));
+
+    @Unique private static Widget MakeDisabledWidget(
+        GridWidget.Adder Instance,
+        Widget W,
+        Operation<Widget> Orig,
+        Tooltip Message
+    ) {
+        Orig.call(Instance, W);
+        var BW = ((ButtonWidget)W);
+        BW.active = false;
+        BW.setTooltip(Message);
+        return W;
+    }
 
     /** We don’t support LAN multiplayer, so disable the button. */
     @WrapOperation(
@@ -25,15 +39,24 @@ public abstract class GameMenuScreenMixin {
             ordinal = 5
         )
     )
-    private Widget inject$initWidgets(
+    private Widget inject$initWidgets$0(
         GridWidget.Adder Instance,
         Widget W,
         Operation<Widget> Orig
-    ) {
-        Orig.call(Instance, W);
-        var BW = ((ButtonWidget)W);
-        BW.active = false;
-        BW.setTooltip(TT);
-        return W;
-    }
+    ) { return MakeDisabledWidget(Instance, W, Orig, LAN_DISABLED); }
+
+    /** Likewise, disable the social interactions screen. */
+    @WrapOperation(
+        method = "initWidgets",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;",
+            ordinal = 6
+        )
+    )
+    private Widget inject$initWidgets$1(
+        GridWidget.Adder Instance,
+        Widget W,
+        Operation<Widget> Orig
+    ) { return MakeDisabledWidget(Instance, W, Orig, REPORT_SCREEN_DISABLED); }
 }
