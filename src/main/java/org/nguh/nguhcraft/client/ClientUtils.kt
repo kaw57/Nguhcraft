942bbe1f50bba52b9ae718a26197548d74f90858
Sirraide <aeternalmail@gmail.com>
2025-04-27 13:06:17 +0200

    [Client] Move emoji replacements into a JSON file

--- src/main/java/org/nguh/nguhcraft/client/ClientUtils.kt
+++ src/main/java/org/nguh/nguhcraft/client/ClientUtils.kt
@@ -1,5 +1,8 @@
 package org.nguh.nguhcraft.client
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.serialization.json.Json
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.client.MinecraftClient
@@ -18,71 +21,9 @@ object ClientUtils {
     const val MAX_CHAT_LENGTH = 2000
 
     /** Emojis we support. */
-    val EMOJI_REPLACEMENTS = mapOf(
-        "gold_nguh" to '\uE200',
-        "rainbow_nguh" to '\uE201',
-        "trangs_nguh" to '\uE202',
-        "ngold_nguh" to '\uE203',
-        "red_nguh" to '\uE204',
-        "red_ng" to '\uE205',
-        "red_schwa" to '\uE206',
-        "red_amogus" to '\uE207',
-        "bigyus" to '\uE208',
-        "emoji_1" to '\uE209',
-        "emoji_2" to '\uE20A',
-        "true_anguish" to '\uE20B',
-        "worse_anguish" to '\uE20C',
-        "worst_anguish" to '\uE20D',
-        "true_joy" to '\uE20E',
-        "better_joy" to '\uE20F',
-        "hellnaw" to '\uE210',
-        "hotspot" to '\uE211',
-        "pridespot" to '\uE212',
-        "trangspot" to '\uE213',
-        "ultrafrenchspot" to '\uE214',
-        "belgianspot" to '\uE215',
-        "lesbianspot" to '\uE216',
-        "coldspot" to '\uE217',
-        "toki_returna" to '\uE218',
-        "trollkipona" to '\uE219',
-        "ough" to '\uE21A',
-        "eigh" to '\uE21B',
-        "blough" to '\uE21C',
-        "lol" to '\uE21D',
-        "newXD" to '\uE21E',
-        "TCPain" to '\uE21F',
-        "EYES" to '\uE220',
-        "ooooooo" to '\uE221',
-        "OOOOOOO" to '\uE222',
-        "ellers_estrogen" to '\uE223',
-        "colon3" to '\uE224',
-        "kek" to '\uE225',
-        "hehH" to '\uE226',
-        "ap" to '\uE227',
-        "predator" to '\uE228',
-        "duolingun" to '\uE229',
-        "thonkening" to '\uE22A',
-        "antistrut" to '\uE22B',
-        "antihmidhat" to '\uE22C',
-        "antierhua" to '\uE22D',
-        "esperandont" to '\uE22E',
-        "ngascended" to '\uE22F',
-        "reaksi_gue" to '\uE230',
-        "smork" to '\uE231',
-        "stroke" to '\uE232',
-        "uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh" to '\uE233',
-        "regional_indicator_ash" to '\uE234',
-        "regional_indicator_eth" to '\uE235',
-        "regional_indicator_thorn" to '\uE236',
-        "silly_me" to '\uE237',
-        "ramsey" to '\uE238',
-        "anti_ramsey" to '\uE239',
-        "nb_nguh" to '\uE23A',
-        "CENSORED" to '\uE23B',
-        "aight" to '\uE23C',
-        "curst" to '\uE23D',
-        "kjehkjeh" to '\uE23E',
-        "whatdidijustread" to '\uE23F',
+    val EMOJI_REPLACEMENTS: Map<String, String> = Gson().fromJson(
+        ClientUtils.javaClass.getResource("/assets/nguhcraft/emoji_replacements.json")!!.readText(),
+        object : TypeToken<Map<String, String>>() {}.type
     )
 
     /** Get the client instance. */
