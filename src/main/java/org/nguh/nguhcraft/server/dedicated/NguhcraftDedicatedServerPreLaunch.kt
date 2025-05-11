d55be3215d15aa167131e7bef41ab9f1ca055402
Sirraide <aeternalmail@gmail.com>
2025-05-11 19:16:45 +0200

    [Server] Actually write to the log file in our custom logger

--- src/main/java/org/nguh/nguhcraft/server/dedicated/NguhcraftDedicatedServerPreLaunch.kt
+++ src/main/java/org/nguh/nguhcraft/server/dedicated/NguhcraftDedicatedServerPreLaunch.kt
@@ -12,6 +12,8 @@ import org.apache.logging.log4j.core.appender.rewrite.RewritePolicy
 import org.apache.logging.log4j.core.layout.PatternLayout
 import org.nguh.nguhcraft.server.ServerUtils.IsDedicatedServer
 import org.nguh.nguhcraft.server.dedicated.NguhcraftDedicatedServer.Companion.ReplaceLogger
+import java.io.File
+import java.io.FileOutputStream
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -25,6 +27,8 @@ class NguhcraftAppender(Rewriter: RewriteAppender? = null) : AbstractAppender(
     false,
     arrayOf()
 ) {
+    val LogFile = FileOutputStream(File("logs/latest.log"), true)
+
     // The rewrite policy and rewriter function are from the stack deobfuscation mod;
     // this way, we can use it without having to depend on it during the build process.
     val Policy = run {
@@ -49,14 +53,21 @@ class NguhcraftAppender(Rewriter: RewriteAppender? = null) : AbstractAppender(
         val E = Policy?.rewrite(Raw) ?: Raw
         val T = LocalDateTime.ofInstant(Instant.ofEpochSecond(E.instant.epochSecond), ZoneOffset.systemDefault())
         val C = E.source.className
+        val Date = T.format(DTF)
+        val Msg = layout.toSerializable(E).toString()
         var Class = (ClassNameRewriter?.invoke(C) ?: C).split('.').last()
         if (Class.endsWith("\$Companion")) Class = Class.dropLast("\$Companion".length)
+
+        // Print message with colours to stdout.
         print(
-            "\u001b[34m[${T.format(DTF)}]\u001b[m " +
+            "\u001b[34m[${Date}]\u001b[m " +
             "\u001b[${ThreadFormat(E.level)}m[${E.threadName}/${E.level}]\u001b[m " +
             "\u001b[36m($Class)\u001b[m " +
-            "\u001b[${MsgFormat(E.level)}m${layout.toSerializable(E)}\u001b[m"
+            "\u001b[${MsgFormat(E.level)}m$Msg\u001b[m"
         )
+
+        // And to the log file w/o colours.
+        LogFile.write("[${Date}] [${E.threadName}/${E.level}] ($Class) $Msg".toByteArray(Charsets.UTF_8))
     }
 
     companion object {
