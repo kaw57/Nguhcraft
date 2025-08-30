a4a7077738e4e01d2f989c0c5807bc7cf2f2b5b8
Sirraide <aeternalmail@gmail.com>
2025-08-30 20:19:40 +0200

    [Client] Render more armour points

--- src/main/java/org/nguh/nguhcraft/mixin/client/render/InGameHudMixin.java
+++ src/main/java/org/nguh/nguhcraft/mixin/client/render/InGameHudMixin.java
@@ -1,16 +1,24 @@
 package org.nguh.nguhcraft.mixin.client.render;
 
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
 import org.nguh.nguhcraft.Utils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static org.nguh.nguhcraft.Nguhcraft.Id;
+
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
+    @Unique private static final Identifier ARMOR_GOLD_FULL_TEXTURE = Id("hud/armour_gold_full");
+    @Unique private static final Identifier ARMOR_GOLD_HALF_TEXTURE = Id("hud/armour_gold_half");
+
     /**
     * Don’t render the hunger bar if the saturation enchantment is maxed out.
     * <p>
@@ -29,5 +37,21 @@ public abstract class InGameHudMixin {
         // Do not render it if the saturation level is maxed out.
         if (Utils.CalculateWeightedSaturationEnchantmentValue(P) >= Utils.MAX_SATURATION_ENCHANTMENT_VALUE)
             CI.cancel();
+    }
+
+    /** Render more than 20 armour points, up to a maximum of 40. */
+    @Inject(method = "renderArmor", at = @At("TAIL"))
+    private static void inject$renderArmor(DrawContext Ctx, PlayerEntity P, int Ht, int J, int K, int Wd, CallbackInfo CI) {
+        var Armour = P.getArmor() - 20;
+        if (Armour > 0) {
+            var Y = Ht - (J - 1) * K - 10;
+            for (var N = 0; N < 10; N++) {
+                var X = Wd + N * 8;
+                if (N * 2 + 1 < Armour)
+                    Ctx.drawGuiTexture(RenderPipelines.GUI_TEXTURED, ARMOR_GOLD_FULL_TEXTURE, X, Y, 9, 9);
+                if (N * 2 + 1 == Armour)
+                    Ctx.drawGuiTexture(RenderPipelines.GUI_TEXTURED, ARMOR_GOLD_HALF_TEXTURE, X, Y, 9, 9);
+            }
+        }
     }
 }
