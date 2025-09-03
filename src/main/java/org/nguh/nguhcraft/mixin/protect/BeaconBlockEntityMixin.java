0a6b0314efc12b017154ac476e748ef8bd90e2e3
Sirraide <aeternalmail@gmail.com>
2025-09-03 18:37:03 +0200

    [Regions] Actually implement LockableBlockEntity for beacons

--- src/main/java/org/nguh/nguhcraft/mixin/protect/BeaconBlockEntityMixin.java
+++ src/main/java/org/nguh/nguhcraft/mixin/protect/BeaconBlockEntityMixin.java
@@ -4,15 +4,25 @@ import net.minecraft.block.entity.BeaconBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.ContainerLock;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.nguh.nguhcraft.item.LockableBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import static org.nguh.nguhcraft.item.LockableBlockEntityKt.CheckCanOpen;
 
 @Mixin(BeaconBlockEntity.class)
-public abstract class BeaconBlockEntityMixin {
+public abstract class BeaconBlockEntityMixin implements LockableBlockEntity {
+    @Unique private static final Text BEACON_NAME = Text.translatable("block.minecraft.beacon");
+    @Unique private String Lock = null;
+
+    @Override public @Nullable String Nguhcraft$GetLock() { return Lock; }
+    @Override public void Nguhcraft$SetLockInternal(@Nullable String NewLock) { Lock = NewLock; }
+    @Override public @NotNull Text Nguhcraft$GetName() { return BEACON_NAME; }
+
     @Redirect(
         method = "createMenu",
         at = @At(
@@ -21,6 +31,6 @@ public abstract class BeaconBlockEntityMixin {
         )
     )
     private boolean inject$createMenu$checkUnlocked(PlayerEntity PE, ContainerLock Unused1, Text Unused2) {
-        return CheckCanOpen(((LockableBlockEntity)this), PE, PE.getMainHandStack());
+        return CheckCanOpen(this, PE, PE.getMainHandStack());
     }
 }
